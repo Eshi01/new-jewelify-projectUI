f0be67fd381d0d8a0bacc1b2887eee4687507a08
Eshi01 <eshanidinelka@gmail.com>
2022-10-20 18:01:56 +0530

    Changes of admin preview

--- pages/admin.js
+++ pages/admin.js
@@ -3,7 +3,7 @@ import BlogPagePreview from '../previews/BlogPagePreview'
 
 const Admin = () => {
   useEffect(() => {
-    ;(async () => {
+    (async () => {
       const CMS = (await import('netlify-cms-app')).default
       CMS.init()
       // hook our preview up to the cms
